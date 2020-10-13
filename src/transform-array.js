2a140624ada836620107d48a559e02ebad7ee32b
AlinaFuchs <kuzmenkoalina.dev@gmail.com>
2020-10-13 13:52:39 +0300

    feat: add solution for Hanoi-tower

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(arr !== Array) {
+
+  }throw new Error('Not implemented');
+  
+  
 };
